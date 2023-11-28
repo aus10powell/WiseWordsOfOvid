1e6fa3f7e325038be7d48e65d54ac966ae16f4e4
Austin Powell <phystron@gmail.com>
2023-11-28 10:52:53 -0800

    oauth2 pkce

--- bot.py
+++ bot.py
@@ -1,34 +1,18 @@
-
 import tweepy
 import configparser
 
 # Create a ConfigParser object
-config = configparser.ConfigParser()
+config = configparser.RawConfigParser()
 
 # Read the INI file
-config.read('config.ini')
+config.read("config.ini")
 
-# Access values from the INI file
-username = config.get('Settings', 'username')
-password = config.get('Settings', 'password')
+# Access values from the sections
+twitter_api_section = config["TwitterAPI"]
+# Get keys
+bearer_token = twitter_api_section["bearer_token"]
+access_token = twitter_api_section["access_token"]
+access_token_secret = twitter_api_section["access_token_secret"]
+client_id = twitter_api_section["client_id"]
+client_secret = twitter_api_section["client_secret"]
 
-server_host = config.get('Server', 'host')
-server_port = config.getint('Server', 'port')  # Use getint to retrieve integer values
-
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-# Example: Post a tweet
-api.update_status("Hello, world!")
-
-# Example: Follow a user
-api.create_friendship("username")
-
-# Example: Reply to a mention
-for mention in api.mentions_timeline():
-    if mention.text.startswith("@your_username"):
-        reply = "@{} Thanks for the mention!".format(mention.user.screen_name)
-        api.update_status(reply, mention.id)
